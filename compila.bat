9c92f410eafd877e4bfe0bbc1256f0bca78912b1
Davide Fasolo <43911192+DavideFasolo@users.noreply.github.com>
2018-10-14 17:07:09 +0200

    Delete compila.bat

--- compila.bat
+++
@@ -1,3 +0,0 @@
-pyinstaller.exe --workpath=comp --distpath="comp" --clean --specpath=comp --onefile -w --icon="Configurazione\icns\icona.ico" HTG.py
-pyinstaller.exe --workpath=comp --distpath="comp" --clean --specpath=comp --onefile -w --icon="Configurazione\icns\icona.ico" splash.py
-xcopy /Y comp\HTG.exe %HOMEPATH%\OneDrive\Desktop\
