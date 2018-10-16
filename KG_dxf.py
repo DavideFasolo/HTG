329b36c53ba497bd2e1b5b75d4e15ebe4a4ddbfb
DavideFasolo <43911192+blueskid83@users.noreply.github.com>
2018-10-16 03:38:44 +0200

    new class
    create nuove classi per le entità, più una macroclasse "Forotag" che racchiude tutti i parametri del foro

--- KG_dxf.py
+++ KG_dxf.py
@@ -1,3 +1,4 @@
+import math
 class punto:
     def __init__(proprio,cX,cY,cZ=0):
         proprio.x=cX
@@ -13,7 +14,6 @@ class linea:
         proprio.dxfcode += '\n10\n'
         proprio.dxfcode += str(x1)
         proprio.dxfcode += '\n20\n'
-        proprio.dxfcode += '0\nLINE\n8\n'
         proprio.dxfcode += str(y1)
         proprio.dxfcode += '\n11\n'
         proprio.dxfcode += str(x2)
@@ -25,8 +25,8 @@ class linea:
         proprio.dxfcode += '\n'
 
 
-class cerchio:
-    def __init__(proprio,cx, cy, cz, cr, livello, colore=0):
+class Cerchio:
+    def __init__(proprio,cx, cy, cz, cr, livello, colore='0'):
         proprio.centro=punto(float(cx),float(cy),float(cz))
         proprio.raggio=float(cr)
         proprio.diametro=cr*2
@@ -45,26 +45,108 @@ class cerchio:
             proprio.dxfcode += colore
         proprio.dxfcode += '\n'
 
-def dxf_testo(cx, cy, cz, ofx, ofy, caratteri, h, livello, colore=0, spessore=0):
-    testo = '0\nTEXT\n8\n'
-    testo += livello
-    testo += '\n10\n'
-    testo += str(cx + ofx)
-    testo += '\n20\n'
-    testo += str(cy + ofy)
-    testo += '\n30\n'
-    testo += str(cz)
-    testo += '\n40\n'
-    testo += h
-    testo += '\n1\n'
-    testo += caratteri
-    if colore:
-        testo += '\n62\n'
-        testo += colore
-    testo += '\n8\n'
-    testo += livello
-    if spessore:
-        testo += '\n39\n'
-        testo += spessore
-    testo += '\n'
-    return testo;
+class Testo:
+    def __init__(proprio,cx, cy, ofx, ofy, stringa, h, livello, colore='0'):
+        proprio.padx=float(ofx)
+        proprio.pady=float(ofy)
+        proprio.valore=str(stringa)
+        proprio.caratteri=len(str(stringa))
+        proprio.dimensione=float(h)
+        proprio.origine=punto(float(cx+proprio.padx),float(cy+proprio.pady))
+        proprio.dxfcode = '0\nTEXT\n8\n'
+        proprio.dxfcode += livello
+        proprio.dxfcode += '\n10\n'
+        proprio.dxfcode += str(proprio.origine.x)
+        proprio.dxfcode += '\n20\n'
+        proprio.dxfcode += str(proprio.origine.y)
+        proprio.dxfcode += '\n40\n'
+        proprio.dxfcode += str(proprio.dimensione)
+        proprio.dxfcode += '\n1\n'
+        proprio.dxfcode += str(proprio.valore)
+        if colore:
+            proprio.dxfcode += '\n62\n'
+            proprio.dxfcode += colore
+        proprio.dxfcode += '\n'
+
+class Etichetta:
+    def __init__(proprio, nume=100, htesto=3.5, px=0, py=0, pz=0, angolo=20, coloretik='0', livelloetik='etichette', appros=4):
+        dmodA=2.6
+        dmodB=2.1
+        dmodC=1.6
+        htesto = float(htesto)
+        if float(nume) > 99:
+            dtesto = htesto * dmodA
+        else:
+            if float(nume) > 9:
+                dtesto = htesto * dmodB
+            else:
+                dtesto = htesto * dmodC
+        proprio.lato = round(dtesto * math.tan(math.pi / 8), 4)
+        proprio.P0 = punto(round(px,appros),
+                           round(py,appros),
+                           round(pz,appros))
+        proprio.P1 = punto(round(px + proprio.lato * math.cos(angolo * math.pi / 180) - proprio.lato / 2, appros),
+                            round(py + proprio.lato * math.sin(angolo * math.pi / 180), appros),
+                            proprio.P0.z)
+        proprio.P2 = punto(round(proprio.P1.x - proprio.lato * math.sin(math.pi / 4), appros),
+                           round(proprio.P1.y + proprio.lato * math.sin(math.pi / 4), appros),
+                           proprio.P0.z)
+        proprio.P3 = punto(round(proprio.P2.x, appros),
+                           round(proprio.P2.y + proprio.lato, appros),
+                           proprio.P0.z)
+        proprio.P4 = punto(round(proprio.P3.x + proprio.lato * math.sin(math.pi / 4), appros),
+                           round(proprio.P3.y + proprio.lato * math.sin(math.pi / 4), appros),
+                           proprio.P0.z)
+        proprio.P5 = punto(round(proprio.P4.x + proprio.lato, appros),
+                           round(proprio.P4.y, appros),
+                           proprio.P0.z)
+        proprio.P6 = punto(round(proprio.P5.x + proprio.lato * math.sin(math.pi / 4), appros),
+                           round(proprio.P5.y - proprio.lato * math.sin(math.pi / 4), appros),
+                           proprio.P0.z)
+        proprio.P7 = punto(round(proprio.P6.x, appros),
+                           round(proprio.P6.y - proprio.lato, appros),
+                           proprio.P0.z)
+        proprio.P8 = punto(round(proprio.P7.x - proprio.lato * math.sin(math.pi / 4), appros),
+                           round(proprio.P7.y - proprio.lato * math.sin(math.pi / 4), appros),
+                           proprio.P0.z)
+        proprio.P9 = punto(proprio.P0.x, proprio.P0.y, proprio.P0.z)
+
+        if nume>9:
+            proprio.padx=proprio.lato / math.sqrt(2) /2.8
+        else:
+            proprio.padx = proprio.lato / math.sqrt(2) / 2.3
+        proprio.pady=0-(htesto-proprio.lato)/2
+        i = 0
+        proprio.dxfcode = str()
+        while i < 9:
+            proprio.dxfcode += '0\nLINE\n8\n'
+            proprio.dxfcode += livelloetik
+            proprio.dxfcode += '\n10\n'
+            proprio.dxfcode += str(eval('proprio.P%d.x' % i))
+            proprio.dxfcode += '\n20\n'
+            proprio.dxfcode += str(eval('proprio.P%d.y' % i))
+            proprio.dxfcode += '\n11\n'
+            proprio.dxfcode += str(eval('proprio.P%d.x' % (i + 1)))
+            proprio.dxfcode += '\n21\n'
+            proprio.dxfcode += str(eval('proprio.P%d.y' % (i + 1)))
+            proprio.dxfcode += '\n62\n'
+            proprio.dxfcode += coloretik
+            proprio.dxfcode += '\n'
+            i += 1
+
+class Forotag:
+    def __init__(proprio,cx=0, cy=0, cz=0, raggioforo=2, htesto=3.5, angolo=20, nume=2,appros=4, colorecer='0',
+                 livellocer='cerchi',coloretik='0',livelloetik='etichette',livellotes='testo',coloretes='0'):
+        proprio.centro=punto(round(cx,appros),round(cy,appros),round(cz,appros))
+        proprio.cerchio=Cerchio(proprio.centro.x,proprio.centro.y,proprio.centro.z,raggioforo,livellocer,colorecer)
+        proprio.etichetta=Etichetta(nume,htesto,
+                                    round(proprio.centro.x + proprio.cerchio.raggio * math.cos(angolo * math.pi / 180), appros),
+                                    round(proprio.centro.y + proprio.cerchio.raggio * math.sin(angolo * math.pi / 180), appros),
+                                    proprio.centro.z,angolo,coloretik,livelloetik,appros)
+        proprio.testo=Testo(proprio.etichetta.P2.x,proprio.etichetta.P2.y,
+                            proprio.etichetta.padx,proprio.etichetta.pady,nume,htesto,livellotes,coloretes)
+
+        proprio.dxfcode=proprio.cerchio.dxfcode+proprio.etichetta.dxfcode+proprio.testo.dxfcode
+
+foroforo=Forotag()
+print(foroforo.dxfcode)
--- outdxf.py
+++ outdxf.py
@@ -3,55 +3,53 @@ from KG_dxf import *
 from infrastructure import DrawingExchangeFormatConfigurations
 
 
-def esporta_dxf(fori,f, workpath):
+def esporta_dxf(fori, f, workpath):
     cf = DrawingExchangeFormatConfigurations(workpath)
-
-    cdf=f+"dxf"
-    out_dxf=open(cdf,"w+")
-    out_dxf.write('999\nFile "'+cdf+'" esportato tramite "Hole Table Generator" by "Kill Goliath"\n')
+    cdf = f + "dxf"
+    out_dxf = open(cdf, "w+")
+    out_dxf.write('999\nFile "' + cdf + '" esportato tramite "Hole Table Generator" by "Kill Goliath"\n')
     out_dxf.write('999\nPowered by Davide Fasolo 2018\n')
     out_dxf.write('0\nSECTION\n')
     out_dxf.write('2\nENTITIES\n')
-
-    i=0
-    while i<len(fori):
-        cr=fori[i][0]/2
-        cx=fori[i][1]
-        cy=fori[i][2]
+    i = 0
+    minx=fori[i][1]-fori[i][0]/2
+    maxx = fori[i][1] + fori[i][0] / 2
+    miny = fori[i][2] - fori[i][0] / 2
+    maxy = fori[i][2] + fori[i][0] / 2
+    while i < len(fori):
+        cr = fori[i][0] / 2
+        cx = fori[i][1]
+        cy = fori[i][2]
         if cf.coord_z:
-            cz=fori[i][3]
+            cz = fori[i][3]
         else:
-            cz=0
-        i+=1
-        # cerchio1=cerchio(cx, cy, cz, cr, cf.cer_livello, cf.cer_colore)
-        # out_dxf.write(cerchio1.dxfcode)
-        exec('cerchio%d = cerchio(%f,%f,%f,%f,"%s","%s")' % (i, cx, cy, cz, cr, cf.cer_livello, cf.cer_colore))
-        exec ("out_dxf.write(cerchio%d.dxfcode)" % i)
-        out_dxf.write(dxf_testo(coord_testo_etichetta(cx,cy,cz,cr, cf.htesto, cf.angolo,'x',i, cf.etk_dmodA, cf.etk_dmodB, cf.etk_dmodAx, cf.etk_dmodBx),coord_testo_etichetta(cx,cy,cz,cr, cf.htesto, cf.angolo,'y',i, cf.etk_dmodA, cf.etk_dmodB, cf.etk_dmodAy, cf.etk_dmodBy,),cz,0,0,str(i), cf.htesto, cf.tes_livello, cf.tes_colore))
-        out_dxf.write(dis_etichetta(cx,cy,cz,cr, cf.htesto, cf.angolo, cf.etk_livello,i, cf.etk_colore, cf.etk_dmodA, cf.etk_dmodB))
-    ###assi
-    g=0
-    maxx=fori[g][1]
-    maxy=fori[g][2]
-    minx=fori[g][1]
-    miny=fori[g][2]
-    while g<=len(fori)-1:
-        if (fori[g][1]>maxx):
-                maxx=fori[g][1]
-        if (fori[g][2]>maxy):
-                maxy=fori[g][2]
-        if (fori[g][1]<minx):
-                minx=fori[g][1]
-        if (fori[g][2]<miny):
-                miny=fori[g][2]
-        g+=1
-    if miny>0:
-        miny=0
-    assex=linea(minx,0,maxx,0, cf.cer_livello, cf.ass_colore)
-    assey=linea(0,maxy,0,miny, cf.cer_livello, cf.ass_colore)
+            cz = 0
+        i += 1
+        if cx-cr<minx:
+            minx=cx-cr
+        if cx+cr>maxx:
+            maxx=cx+cr
+        if cy-cr<miny:
+            miny=cy-cr
+        if cy+cr>maxy:
+            maxy=cy+cr
+        foro1=Forotag(cx,cy,cz,cr,cf.htesto,cf.angolo,i,4,cf.cer_colore,cf.cer_livello,
+                      cf.etk_colore,cf.etk_livello,cf.tes_livello,cf.tes_colore)
+        exec('foro%d = Forotag(%f,%f,%f,%f,%f,%f,%d,4,%s,"%s",%s,"%s","%s",%s)' % (i,cx,cy,cz,cr,
+                                                                                      cf.htesto,cf.angolo,i,
+                                                                                      cf.cer_colore,cf.cer_livello,
+                                                                                      cf.etk_colore,cf.etk_livello,
+                                                                                      cf.tes_livello,cf.tes_colore))
+        exec("out_dxf.write(foro%d.dxfcode)" % i)
+        out_dxf.write(
+            dis_etichetta(cx, cy, cz, cr, cf.htesto, cf.angolo, cf.etk_livello, i, cf.etk_colore, cf.etk_dmodA,
+                          cf.etk_dmodB))
+
+    assex = linea(minx, 0, maxx, 0, cf.cer_livello, cf.ass_colore)
+    assey = linea(0, maxy, 0, miny, cf.cer_livello, cf.ass_colore)
     out_dxf.write(assex.dxfcode)
     out_dxf.write(assey.dxfcode)
     #######
     out_dxf.write('0\nENDSEC\n0\nEOF')
     out_dxf.close()
-    print("Creato il file \n"+cdf+"\n"+"Nella cartella dove si trova il file vda selezionato")
+    print("Creato il file \n" + cdf + "\n" + "Nella cartella dove si trova il file vda selezionato")
