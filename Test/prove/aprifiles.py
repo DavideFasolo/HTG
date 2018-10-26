135e86e1042f4e942332dc3ef18a69b288401556
DavideFasolo <43911192+blueskid83@users.noreply.github.com>
2018-10-26 13:49:51 +0200

    work in progress

--- Test/prove/aprifiles.py
+++ Test/prove/aprifiles.py
@@ -1,17 +1,21 @@
-class OpenFile:
-    def __init__(self,filename):
-        self.filename = filename
-        self.p = str()
-    def set_p(self):
-        self.p = (i==0 and self.p + self.filename.split('/')[i]) or (i!=0 and self.p + '/' + self.filename.split('/')[i])
-    def get_path(self):
-        k = len(self.filename.split('/'))
-        for i in range(0, k - 1):
-            self.p = (i==0 and self.p + self.filename.split('/')[i]) or (i!=0 and self.p + '/' + self.filename.split('/')[i])
-        return self.p
+filename = 'C:/drawing/cad/tabelle fori XYZ/Hole Table Generator/sorgenti/filetestarea/test.vda'
+p = str()
+print(filename.split('/'))
+
+
+def add_slash(s): return str(s) + '/'
+
+
+def concat(s): p += s
+
+
+def set_p(p):
+    list(map(add_slash, filename.split('/')))
 
 #        for e in lst:  func(e)
 #        map(func,lst)
 
-newfile = OpenFile('C:/drawing/cad/tabelle fori XYZ/Hole Table Generator/sorgenti/filetestarea/test.vda')
-print(newfile.get_path())
+
+map(concat, list(map(add_slash, filename.split('/'))))
+print(p)
+
